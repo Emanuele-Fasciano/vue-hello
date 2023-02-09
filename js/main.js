ecaee29cf1023e903e984218fb31eccb18a5f600
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-02-09 16:30:11 +0100

    end exercise

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+const app = Vue.createApp({
+
+    data(){  
+        return{
+            title: "Spiderman 2",
+            textClass: "display-1",
+            img: "./img/01.webp"
+        }
+    },
+
+});
+
+app.mount("#root")
+
+// console.log(Vue);
